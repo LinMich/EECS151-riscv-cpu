9b2598f678e84a5e86fe0b881135690008311384
David Dominguez Hooper <davidhooper4@berkeley.edu>
2018-04-01 21:23:39 -0700

    added reg file

--- hardware/src/riscv_core/reg_file.v
+++ hardware/src/riscv_core/reg_file.v
@@ -5,6 +5,24 @@ module reg_file (
     input [31:0] wd,
     output [31:0] rd1, rd2
 );
-    assign rd1 = 32'd0;
-    assign rd2 = 32'd0;
+	(* ram_style = "distributed" *) reg [31:0] register [31:0];
+
+    assign rd1 = register[ra1];
+    assign rd2 = register[ra2];
+
+    integer i;
+
+    always @(posedge clk) begin
+    	if (reset) begin
+    		for (i=0; i<32; i=i+1) begin
+    			register[i] <= 32'd0;
+    		end
+    	end
+    	else if (we) begin
+    		if (wa != 0) begin
+    			register[wa] <= wd;
+    		end
+    	end
+    end
+
 endmodule
