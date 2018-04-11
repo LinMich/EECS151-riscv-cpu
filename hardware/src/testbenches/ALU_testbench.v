43a04e9a031505e316dce1ce7b0500c4d6a57792
davidh- <davidhooper4@berkeley.edu>
2018-04-10 17:05:52 -0700

    Fixed vivado source files

--- hardware/src/testbenches/ALU_testbench.v
+++ hardware/src/testbenches/ALU_testbench.v
@@ -1,2 +1,131 @@
 `include "Opcode.vh"
 
+`timescale 1ns / 1ps
+
+module ALU_testbench();
+    parameter period = 20; // 20ns period for 50MHz clk
+    
+    reg clk;
+    
+    // generate clock signals
+    initial clk = 0;
+    always #(period/2) clk = ~clk;
+    
+    // registers and wires to test ALU
+    reg [31:0] ina;
+    reg [31:0] inb;
+    
+    reg [2:0] fnc3;
+    reg fnc1;
+    
+    wire [31:0] result;
+    
+    reg [31:0] result_REF;
+    // instantiate ALU module
+    ALU alu(
+        .ina(ina),
+        .inb(inb),
+        .fnc3(fnc3),
+        .fnc1(fnc1),
+        .result(result));
+
+    integer i;
+    
+    reg [30:0] rand_31;
+    reg [14:0] rand_15;
+    reg [31:0] A, B;
+    reg[4*8:0] type;
+    
+    task checkOutput;
+    input reg[4*8:0] type;
+        if (result == result_REF)
+            $display("PASS %s: \tA: 0x%h, B: 0x%h, result: 0x%h, result_REF: 0x%h", type, A, B, result, result_REF);
+        else begin
+            $display("FAIL %s: \tA: 0x%h, B: 0x%h, result: 0x%h, result_REF: 0x%h", type, A, B, result, result_REF);
+            $finish();
+        end
+    endtask
+    initial begin
+        // reset all parts
+        ina = 32'b0;
+        inb = 32'b0;
+        fnc3 = 3'b0;
+        fnc1 = 1'b0;
+        for(i = 0; i < 20; i = i + 1) begin
+             $display("Round %d", i);
+            #1;
+            // Make both A and B negative to check signed operations
+            rand_31 = {$random} & 31'h7FFFFFFF;
+            rand_15 = {$random} & 15'h7FFF;
+            A = {1'b1, rand_31};
+            // Hard-wire 16 1's in front of B for sign extension
+            B = {16'hFFFF, 1'b1, rand_15};
+            ina = A;
+            inb = B;  
+                      
+            //Test ADD:
+            type = "ADD";
+            fnc3 = `FNC_ADD_SUB;
+            fnc1 = `FNC2_ADD;
+            result_REF = A + B;
+            #1
+            checkOutput(type);
+            //Test SUB:
+            type = "SUB";
+            fnc1 = `FNC2_SUB;
+            result_REF = A - B;
+            #1         
+            checkOutput(type);
+            //Test SLL:
+            type = "SLL";
+            fnc3 = `FNC_SLL;
+            result_REF = A << B[4:0];
+            #1         
+            checkOutput(type);
+            //Test SLT:
+            type = "SLT";
+            fnc3 = `FNC_SLT;
+            result_REF = $signed(A) < $signed(B);  
+            #1         
+            checkOutput(type);
+            //Test SLTU:
+            type = "SLTU";
+            fnc3 = `FNC_SLTU;
+            result_REF = A < B;
+            #1         
+            checkOutput(type);
+            //Test XOR:
+            type = "XOR";
+            fnc3 = `FNC_XOR;
+            result_REF = A ^ B;
+            #1         
+            checkOutput(type);
+            //Test OR:
+            type = "OR";
+            fnc3 = `FNC_OR;
+            result_REF = A | B;
+            #1         
+            checkOutput(type);
+            //Test AND:
+            type = "AND";
+            fnc3 = `FNC_AND;
+            result_REF = A & B;
+            #1         
+            checkOutput(type);
+            //Test SRL:
+            type = "SRL";
+            fnc3 = `FNC_SRL_SRA;
+            fnc1 = `FNC2_SRL;
+            result_REF = A >> B[4:0];
+            #1         
+            checkOutput(type);
+            //Test SRA:
+            type = "SRA";
+            fnc1 = `FNC2_SRA;
+            result_REF = ($signed(A) >>> B[4:0]);                      
+            #1         
+            checkOutput(type);    
+        end
+        $finish();
+    end
+endmodule 
--- hardware/src/testbenches/assembly_testbench.v
+++ hardware/src/testbenches/assembly_testbench.v
@@ -1,12 +1,13 @@
 `timescale 1ns/10ps
 
 /* MODIFY THIS LINE WITH THE HIERARCHICAL PATH TO YOUR REGFILE ARRAY INDEXED WITH reg_number */
+//`define REGFILE_ARRAY_PATH CPU.dpath.RF.reg_file[reg_number]
 `define REGFILE_ARRAY_PATH CPU.reggie.register[reg_number]
 
 module assembly_testbench();
     reg clk, rst;
     parameter CPU_CLOCK_PERIOD = 20;
-    parameter CPU_CLOCK_FREQ = 50_000_000;
+    parameter CPU_CLOCK_FREQ = 1_000_000;
 
     initial clk = 0;
     always #(CPU_CLOCK_PERIOD/2) clk <= ~clk;
@@ -46,19 +47,214 @@ module assembly_testbench();
 
         // Reset the CPU
         rst = 1;
-        repeat (30) @(posedge clk);             // Hold reset for 30 cycles
+        repeat (15) @(posedge clk);             // Hold reset for 30 cycles
         rst = 0;
 
+        $display("beginning!");
         // Your processor should begin executing the code in /software/assembly_tests/start.s
+        
+        
+//        // Test U-type: 
+//        // Test LUI
+//        wait_for_reg_to_equal(16, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd4096, 1);               // Verify that x1 contains 4096      
+        
+//        // Test AUIPC
+//        wait_for_reg_to_equal(17, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'h40001008, 1);               // Verify that x1 contains 2      
+//        // NOT SURE WHETHER THIS IS SUPPOSED TO BE PC OF INSTRUCTION OR CURRENT PC VAL.
+        
+               
+               
+               
+//        // Test J-type: 
+//        // Test JAL
+//        wait_for_reg_to_equal(18, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd10, 1);               // Verify that x1 contains 2
+                
+                
+                
+                
+//        // Test I-type: 
+//        // Test JALR
+//        wait_for_reg_to_equal(19, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(11, 32'd10, 1);               // Verify that x11 contains 10
+        
+//        // Test LB
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'hC0000001, 1);               // Verify that x1 contains 0xC0000001
+        
+//        // Test LH
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd4, 1);               // Verify that x1 contains 4
+        
+//        // Test LW
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd2, 1);               // Verify that x1 contains 2
+        
+//        // Test LBU
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'hC0000001, 1);               // Verify that x1 contains 0xC0000001
+        
+//        // Test LHU
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd4, 1);               // Verify that x1 contains 4
+        
+//        // Test ADDI
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(2, 2048, 1);               // Verify that x1 contains 2048
+        
+//        // Test SLTI
+//        wait_for_reg_to_equal(21, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd1, 1);               // Verify that x1 contains 1
+        
+//        wait_for_reg_to_equal(22, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd0, 1);               // Verify that x1 contains 0
+              
+//        // Test SLTIU
+//        wait_for_reg_to_equal(23, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd0, 1);               // Verify that x1 contains 0
 
-        // Test ADD
-        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
-        check_reg(1, 32'd300, 1);               // Verify that x1 contains 300
+//        wait_for_reg_to_equal(24, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd1, 1);               // Verify that x1 contains 1
+                
+//        // Test XORI
+//        wait_for_reg_to_equal(25, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd2046, 1);               // Verify that x1 contains 2046
+        
+//        // Test ANDI
+//        wait_for_reg_to_equal(26, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd1, 1);               // Verify that x1 contains 1
+        
+//        // Test SLLI
+//        wait_for_reg_to_equal(27, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd4, 1);               // Verify that x1 contains 4
+        
+//        // Test SRLI
+//        wait_for_reg_to_equal(28, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd2, 1);               // Verify that x1 contains 2
+        
+//        // Test SRAI
+//        wait_for_reg_to_equal(29, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'hC0000000, 1);               // Verify that x1 contains 0xC0000001
+        
+        
+        
+                
+//        // Test B-type:    
+//        // Test BEQ
+//        wait_for_reg_to_equal(13, 32'd1);       // Run the simulation until the flag is set to 2
+//        check_reg(1, 32'd6, 1);               // Verify that x1 contains 500
+        
+//        // Test BNE
+//        wait_for_reg_to_equal(14, 32'd1);       // Run the simulation until the flag is set to 2
+//        check_reg(1, 32'd3, 1);               // Verify that x1 contains 500
+        
+//        // Test BLT
+//        wait_for_reg_to_equal(15, 32'd1);       // Run the simulation until the flag is set to 2
+//        check_reg(1, 32'd5, 1);               // Verify that x1 contains 500
+        
+//        // Test BGE
+//        wait_for_reg_to_equal(16, 32'd1);       // Run the simulation until the flag is set to 2
+//        check_reg(1, 32'd7, 1);               // Verify that x1 contains 500
 
-        // Test BEQ
-        wait_for_reg_to_equal(20, 32'd2);       // Run the simulation until the flag is set to 2
-        check_reg(1, 32'd500, 2);               // Verify that x1 contains 500
-        check_reg(2, 32'd100, 3);               // Verify that x2 contains 100
+//        // Test BLTU
+//        wait_for_reg_to_equal(17, 32'd1);       // Run the simulation until the flag is set to 2
+//        check_reg(1, 32'd3, 1);               // Verify that x1 contains 500
+
+//        // Test BGEU
+//        wait_for_reg_to_equal(18, 32'd1);       // Run the simulation until the flag is set to 2
+//        check_reg(1, 32'd5, 1);               // Verify that x1 contains 500
+
+
+//        // Test S-type:    
+//        // Test SB
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd300, 1);               // Verify that x1 contains 300
+        
+//        // Test SH
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd300, 1);               // Verify that x1 contains 300
+        
+        // Test SW
+//        wait_for_reg_to_equal(5, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(0, 32'd0, 1);               // Verify that x1 contains 300
+        
+
+        wait_for_reg_to_equal(5, 32'd1);
+        check_reg(0, 32'd0, 1);
+        
+        wait_for_reg_to_equal(6, 32'd1);
+        check_reg(0, 32'd0, 1);
+        
+        wait_for_reg_to_equal(7, 32'd1); 
+        check_reg(0, 32'd0, 1);
+
+
+
+//        // Test R-type:    
+//        // Test ADD
+//        wait_for_reg_to_equal(30, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd300, 1);               // Verify that x1 contains 300
+        
+//        // Test SUB
+//        wait_for_reg_to_equal(31, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd100, 1);               // Verify that x1 contains 100
+        
+//        //****** Test LW
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(16, 32'd200, 1);               // Verify that x1 contains 2
+        
+//        // Test SLL
+//        wait_for_reg_to_equal(20, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd4, 1);               // Verify that x1 contains 4
+        
+//        // Test SLT
+//        wait_for_reg_to_equal(21, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd1, 1);               // Verify that x1 contains 1
+        
+//        wait_for_reg_to_equal(22, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd0, 1);               // Verify that x1 contains 0
+        
+//        // Test SLTU
+//        wait_for_reg_to_equal(23, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd0, 1);               // Verify that x1 contains 0
+        
+//        wait_for_reg_to_equal(24, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd1, 1);               // Verify that x1 contains 1
+              
+//        // Test XOR
+//        wait_for_reg_to_equal(25, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd6, 1);               // Verify that x1 contains 6
+        
+//        // Test SRL
+//        wait_for_reg_to_equal(26, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd2, 1);               // Verify that x1 contains 2
+        
+//        // Test SRA
+//        wait_for_reg_to_equal(27, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'hC0000000, 1);               // Verify that x1 contains 0xC0000001
+        
+//        // Test OR
+//        wait_for_reg_to_equal(28, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd384, 1);               // Verify that x1 contains 384
+        
+//        // Test AND
+//        wait_for_reg_to_equal(29, 32'd1);       // Run the simulation until the flag is set to 1
+//        check_reg(1, 32'd2046, 1);               // Verify that x1 contains 2046
+        
+        
+
+
+
+
+
+
+
+
+
+
+
 
         $display("ALL ASSEMBLY TESTS PASSED");
         $finish();
